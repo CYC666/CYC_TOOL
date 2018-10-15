7f63cd620a3a77a26730a845da67b8400643578d
CYC666 <13705038428@163.com>
2018-10-15 18:55:23 +0800

    创建项目

---
+++ CYC_TOOL/CYC_TOOL/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CYC_TOOL
+//
+//  Created by 曹老师 on 2018/10/15.
+//  Copyright © 2018年 曹奕程. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
